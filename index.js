a6f112396f33f2f775cefa9097916ebc4310c3f4
EddieOne <eddie@onefoldmedia.com>
2015-04-23 15:04:28 -0600

    add some missing stuff

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ var secret = 'APP SECRET';
 var site = 'http://192.168.56.101:3000/';
 var redirect = 'http://localhost:2900/oauth/';
 
-var template;
-
-fs.readFile( __dirname + '/content.html', 'utf8', function (err, data) {
-	template = data;
-	app.get('/', function (req, res) {
-		var link1 = '<a href="'+site+'/oauth/login?client_id='+clientid+'&response_type=token&redirect_uri='+encodeURIComponent(redirect)+'">Login</a>';
-		var link2 = '<a href="'+site+'/oauth/choose_identity?client_id='+clientid+'&response_type=token&redirect_uri='+encodeURIComponent(redirect)+'">Choose Identity</a>';
+app.get('/', function (req, res) {
+	fs.readFile( __dirname + '/content.html', 'utf8', function (err, data) {
+		var link = '<a href="'+site+'/oauth/login?client_id='+clientid+'&response_type=token&redirect_uri='+encodeURIComponent(redirect)+'">Login</a>';
 		var content = data.split('%%body%%').join(link);
 		res.send(content)
+	})
+});
+
+app.get('/oauth/', function (req, res) {
+	fs.readFile( __dirname + '/content.html', 'utf8', function (err, data) {
+			fs.readFile( __dirname + '/apis.html', 'utf8', function (err, apis) {
+			var content = data.split('%%body%%').join(apis);
+			content = content.split('%%site%%').join(site);
+			res.send(content)
+		})
 	})
 });
 
